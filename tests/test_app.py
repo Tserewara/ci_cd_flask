b9e12b15e00b9c74de44a209416602bb35f49bd2
Tserewara <alvaro.alf2@gmail.com>
2021-02-05 14:19:36 -0300

    Add test for new page

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,7 @@ from src.flask_app import index
 
 def test_index_returns_html():
     assert index() == "<h1>Hello World</h1>"
+
+
+def test_page_returns_html():
+    assert page() == "<h1>Page is here</h1>"
