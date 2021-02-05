f22b074f3292bf8f6ac06586ea4d75a621fd2600
Tserewara <alvaro.alf2@gmail.com>
2021-02-05 15:17:02 -0300

    Add new page

--- src/flask_app.py
+++ src/flask_app.py
@@ -13,6 +13,9 @@ def index():
 def page():
     return "<h1>Page is here</h1>"
 
+@app.route("/about")
+def about():
+    return "<h1>This is a great page</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
